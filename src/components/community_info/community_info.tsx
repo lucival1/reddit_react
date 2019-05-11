aed2d7324afd9c92e885dd374b8c3553cfdf6672
lucivalnascimento <lucival1@gmail.com>
2019-05-11 18:08:18 +0100

    Changes community_info and add search bar

--- src/components/community_info/community_info.tsx
+++ src/components/community_info/community_info.tsx
@@ -4,50 +4,64 @@ import Col from "react-bootstrap/Col";
 import {BlockBtn} from "../buttons/block_btn";
 
 const CommunityInfoBoxStyle: React.CSSProperties = {
-    margin: "10px",
-    border: "3px solid black",
-    borderRadius: "5px",
-    background: "rgb(247, 248, 249)",
-    padding: "10px",
-}
-
-interface ListItem {
-    userId: number;
-    id: number;
-    title: string;
-    url: string,
+  margin: "10px",
+  border: "3px solid black",
+  borderRadius: "5px",
+  background: "rgb(247, 248, 249)",
+  padding: "10px",
 }
 
 interface CommunityInfoProps {
-    items?: ListItem[];
 }
 
 interface CommunityInfoState {
+  count: string;
 }
 
 export class CommunityInfo extends React.Component<CommunityInfoProps, CommunityInfoState> {
-    public render() {
-        return (
-            <Row style={ CommunityInfoBoxStyle }>
-                <Col>
-                    <Row style={{padding: "20px"}}>
-                        <Col xs={1} md={1}>
-                            <i className="fas fa-hat-wizard fa-lg"></i>
-                        </Col>
-                        <Col xs={3} md={3}>
-                            r/cct
-                        </Col>
-                        <Col xs={3} md={3}></Col>
-                        <Col xs={3} md={3}>users</Col>
-                    </Row>
-                    <Row style={{padding: "20px"}}>
-                        <BlockBtn
-                            variant={"outline-dark"}
-                            name={"Create Post"}
-                        />
-                    </Row>
-                </Col>
-            </Row>
-        );
-    }
+  public constructor(state: CommunityInfoState) {
+    super(state);
+    this.state = {
+      count: "0"
+    };
+  }
+
+  public componentWillMount() {
+    (async () => {
+      const data = await getUserCount();
+      this.setState({count: data.count});
+    })();
+  }
+
+  public render() {
+    return (
+      <Row style={CommunityInfoBoxStyle}>
+        <Col>
+          <Row style={{padding: "20px"}}>
+            <Col xs={1} md={1}>
+              <i className="fas fa-hat-wizard fa-lg"></i>
+            </Col>
+            <Col xs={3} md={3}>
+              r/cct
+            </Col>
+            <Col xs={2} md={2}></Col>
+            <Col xs={4} md={4}>{this.state.count} users</Col>
+          </Row>
+          <Row style={{padding: "20px"}}>
+            <BlockBtn
+              variant={"outline-dark"}
+              name={"Create Post"}
+              path={"/link_editor"}
+            />
+          </Row>
+        </Col>
+      </Row>
+    );
+  }
+}
+
+async function getUserCount() {
+  const response = await fetch(`/api/v1/users/count`);
+  const json = await response.json();
+  return json as CommunityInfoState;
 }
